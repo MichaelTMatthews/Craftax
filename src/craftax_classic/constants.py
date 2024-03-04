5cb5cac994e2621ca0009d8c1c3e45ad3c26963a
Michael Beukman <michael.beukman@gmail.com>
2024-03-04 13:19:58 +0000

    Changed the texture cache filenames so that classic and normal craftax do not conflict. Also added `BLOCK_PIXEL_SIZE_IMG` to classic.

--- src/craftax_classic/constants.py
+++ src/craftax_classic/constants.py
@@ -14,9 +14,10 @@ OBS_DIM = (7, 9)
 MAX_OBS_DIM = max(OBS_DIM)
 assert OBS_DIM[0] % 2 == 1 and OBS_DIM[1] % 2 == 1
 BLOCK_PIXEL_SIZE_HUMAN = 64
+BLOCK_PIXEL_SIZE_IMG   = 16
 BLOCK_PIXEL_SIZE_AGENT = 7
 INVENTORY_OBS_HEIGHT = 2
-TEXTURE_CACHE_FILE = os.path.join(pathlib.Path(__file__).parent.resolve(), "assets", "texture_cache.pbz2")
+TEXTURE_CACHE_FILE = os.path.join(pathlib.Path(__file__).parent.resolve(), "assets", "texture_cache_classic.pbz2")
 
 # ENUMS
 class BlockType(Enum):
@@ -435,6 +436,7 @@ if os.path.exists(TEXTURE_CACHE_FILE):
 else:
     TEXTURES = {
         BLOCK_PIXEL_SIZE_AGENT: load_all_textures(BLOCK_PIXEL_SIZE_AGENT),
+        BLOCK_PIXEL_SIZE_IMG:   load_all_textures(BLOCK_PIXEL_SIZE_IMG),
         BLOCK_PIXEL_SIZE_HUMAN: load_all_textures(BLOCK_PIXEL_SIZE_HUMAN),
     }
     save_compressed_pickle(TEXTURE_CACHE_FILE, TEXTURES)
