d358c17647b97fa82547d745cff5717ef13fca3a
Dami <102320150+dami2106@users.noreply.github.com>
2025-09-23 21:07:41 +0200

    Add VecMonitor and update DQN training parameters
    Introduces VecMonitor to the environment for improved logging and monitoring. Updates DQN model hyperparameters, increases total training timesteps, and configures TensorBoard logging for better experiment tracking.

--- dqn_basic_actions.py
+++ dqn_basic_actions.py
@@ -31,6 +31,7 @@ if __name__ == "__main__":
     )
 
     vec_env = VecTransposeImage(vec_env)
+    vec_env = VecMonitor(vec_env)
 
     policy_kwargs = dict(
         net_arch=[256, 256],
@@ -40,8 +41,23 @@ if __name__ == "__main__":
         policy="CnnPolicy",
         env=vec_env,
         verbose=1,
+        learning_rate=1e-4,
+        buffer_size=200,
+        learning_starts=100,
+        batch_size=32,
+        tau=1.0,
+        gamma=0.99,
+        train_freq=(4, "step"),
+        gradient_steps=1,
+        target_update_interval=100,
+        exploration_fraction=0.2,
+        exploration_initial_eps=1.0,
+        exploration_final_eps=0.05,
+        policy_kwargs=policy_kwargs,
+        tensorboard_log="./tb_logs_dqn_craftax",
+        device="auto",
     )
 
-    model.learn(total_timesteps=500, progress_bar=True)
+    model.learn(total_timesteps=20000, log_interval=1, tb_log_name="run1", progress_bar=True)
 
     model.save("dqn_craftax_topdown")
